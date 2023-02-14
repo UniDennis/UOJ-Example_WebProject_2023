a1e817b3a22e3e0d018cb5e62e320ccbad3e8464
Dennis <Dennis@DESKTOP-2OFJRTL>
2023-02-14 20:39:48 +1100

    Add project files.

---
+++ UOJ-Example_WebProject_2023/Pages/Index.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using IBMU2.UODOTNET;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UOJ_Example_WebProject_2023.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly ILogger<IndexModel>? _logger;
+
+        //public IndexModel(ILogger<IndexModel> logger) => _logger = logger;
+
+
+        private string strMessage = "";
+        private string strResponse = "";
+
+		private readonly UOJUniware ee = new();
+        private UniSession? uniSession;
+
+
+        public IndexModel() 
+        {
+            uniSession = ee.pSession;
+        }
+
+        public string Message<T>()
+        { return strMessage; }
+
+        public void SetMessage(string message) { strMessage = message; }
+
+        //public UniSession UniSession { get => uniSession; }
+
+
+
+        public void OnGet()
+        {
+            if( uniSession != null)
+            {
+                strMessage = "Seems to be connected.";
+            }
+            else
+            {
+                strMessage = "uniSession is null.";
+            }
+        }
+
+        public string GetMessage()
+        {
+            return strMessage;
+        }
+		public string GetQueryResponse()
+		{
+			return strResponse;
+		}
+
+		private string iQuery(string query)
+        {
+            //
+            // WARNING THIS IS UNSANITIZED INPUT DATA
+            //
+            // EXPECT SOMEONE TO BREAK EVERYTHING
+            //
+            // DO NOT EVER DO THIS ON A PUBLIC SYSTEM
+            //
+            // HECK!  DON'T EVEN DO IT ON A PRIVATE SYSTEM
+            //
+            if (query.Contains("!")) { return "Nyet!";  }
+
+
+            if (uniSession != null)
+            {
+                UniCommand myCommand = uniSession.CreateUniCommand();
+                myCommand.Command = query;
+                myCommand.Execute();
+                strResponse = myCommand.Response.ToString();
+                return strResponse.Replace(Environment.NewLine, "<br />");
+            }
+            else
+            {
+                strResponse = "Unable to process query.";
+                return strResponse;
+            }
+
+        }
+
+        public void OnPost(string myQuery)
+        {
+			strResponse = iQuery(myQuery);
+        }
+
+    }
+}
---
+++ UOJ-Example_WebProject_2023/UOJUniware.cs
@@ -0,0 +1,55 @@
+﻿using IBMU2.UODOTNET;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Security.Principal;
+
+namespace UOJ_Example_WebProject_2023
+{
+    public class UOJUniware
+    {
+        private string _connectionUser = "webrpc";
+        private string _connectionPass = "@Uniware123!";
+        private IPAddress _connectionHost = IPAddress.Parse("192.168.54.37");
+        private int _connectionPort = int.Parse("31438");
+        private string _connectionDataBase = "/unidata/development/TESTW1";
+        private UniSession? _session;
+
+
+        public UOJUniware()
+        {
+            pSession = Connect();
+        }
+
+
+        public UniSession? pSession
+        {
+            get => _session;
+            set { _session = value; }
+        }
+
+        public UniSession Connect() 
+        {
+            // Get ...
+            try
+            {
+                // Call the Open Session function with our predefined connection variables.
+                _session = UniObjects.OpenSession(_connectionHost.ToString(), 31438, _connectionUser, _connectionPass, _connectionDataBase, "udcs");
+                return _session;
+            }catch(Exception ex)
+            {
+                // Add some proper logging.
+                Console.WriteLine(ex.ToString());
+            }
+            return null;
+        }
+        public void Disconnect() { }
+        public bool IsConnected() { return true; }
+
+
+
+
+
+
+    }
+}
